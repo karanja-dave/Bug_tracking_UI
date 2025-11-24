fd4b8a5ff3cec488b2e5056e6e8186b979145580
karanjadave <davekaranja6@gmail.com>
2025-11-24 20:42:39 +0300

    renamed features pages to producst page

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from '../components/nav/Navbar'
+import { FeaturesInfo } from '../components/products/ProductsInfo'
+import { Footer } from '../components/footer/Footer'
+
+export const FeaturesPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <FeaturesInfo/> 
+      <Footer/> 
+    </div>
+  )
+}
